8d80002a5578b6845b381354185531ec914b9a46
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-11-16 19:08:42 +0530

    add device workflow

---
+++ server/src/cli/main.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import dotenv from "dotenv";
+import chalk from "chalk";
+import figlet from "figlet";
+import { Command, program } from "commander";
+import { login } from "./commands/auth/login.js";
+
+dotenv.config();
+
+async function main() {
+  // Sexy Banner
+  console.log(
+    chalk.hex("#6A5ACD")(
+      figlet.textSync("Maverick", {
+        font: "ANSI Shadow",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 120
+      })
+    )
+  );
+
+  console.log(
+    chalk.greenBright("<---- Intelligent Command Line Interface ---->\n")
+  );
+
+
+  const program = new Command("Maverick");
+  program.version("1.4.3")
+  .description("Maverick Cli - A cli based AI tool")
+  .addCommand(login)
+
+  program.action(()=>{
+    program.help();
+})
+
+program.parse()
+}
+
+
+
+main().catch((err)=>{
+    console.log(chalk.red("Error : Maverick don't wanna shown UP..!!; Because of this ->"),err)
+    process.exit(1)
+});
--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -1,5 +1,6 @@
 import {betterAuth} from "better-auth";
 import {prismaAdapter} from "better-auth/adapters/prisma";
+import { deviceAuthorization } from "better-auth/plugins";
 import prisma from "./db.js"
 
 export const auth = betterAuth({
@@ -13,7 +14,13 @@ export const auth = betterAuth({
             clientId: process.env.GITHUB_CLIENT_ID, 
             clientSecret: process.env.GITHUB_CLIENT_SECRET
         }
-    }
+    },
+     plugins: [ 
+    deviceAuthorization({ 
+      expiresIn: "30m", 
+      interval: "5s", 
+    }), 
+  ],
 });
 
 
