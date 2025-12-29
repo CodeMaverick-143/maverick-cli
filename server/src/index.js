8c4ad5464d3e098ea3edf9301d0173e69d39ab2d
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-12-29 08:27:46 +0530

    feat: Add HEAD / endpoint and update auth route wildcard.

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import { toNodeHandler , fromNodeHeaders} from "better-auth/node";
+import { toNodeHandler, fromNodeHeaders } from "better-auth/node";
 import cors from "cors";
-import {auth} from "./lib/auth.js"
-
+import { auth } from "./lib/auth.js"
 
 dotenv.config()
 
@@ -12,35 +11,37 @@ const app = express()
 // *----------- CORS --------------*
 app.use(
     cors({
-        origin:"http://localhost:3000",
-        methods:["GET","POST","PUT","DELETE"],
-        credentials:true,
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     })
 );
 
-app.all("/api/auth/*splat", toNodeHandler(auth))
-
-
 app.use(express.json());
 
-app.get("/api/me", async(req,res) =>{
+app.all("/api/auth/*", toNodeHandler(auth))
+
+app.get("/api/me", async (req, res) => {
     const session = await auth.api.getSession({
         headers: fromNodeHeaders(req.headers),
     });
     return res.json(session);
 })
+app.head('/', (req, res) => {
+    res.status(200).end();
+});
 
 
-app.get("/device", async(req,res)=>{
-    const {user_code} = req.query
+app.get("/device", async (req, res) => {
+    const { user_code } = req.query
     res.redirect(`http://localhost:3000/device?user_code=${user_code}`)
 })
 
 
-app.get('/health',(req,res)=>{
+app.get('/health', (req, res) => {
     res.send("Backend is running")
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`running on PORT : http://localhost:${process.env.PORT}`)
 })
